004c1010403ffc13c2167284bcc686005865fd1b
Virsel <paul-hornig@gmx.de>
2023-07-17 11:22:45 +0200

    socket integrated

--- src/boot/speech.ts
+++ src/boot/speech.ts
@@ -3,40 +3,62 @@ import {Observable} from "rxjs";
 export default async ({app}) => {
   let recorder: any = null;
 
+
   app.config.globalProperties.$speechToText = {
     start: (lang = "de-DE", message = "", continuous = false) => {
       return new Observable(subscriber => {
-        const handleSuccess = async function (stream) {
-          console.log("micro input", stream)
-          recorder = new MediaRecorder(stream);
-          const data: any = [];
+        const socket = new WebSocket('ws://localhost:8001')
+          socket.onopen = () => {
+            navigator.mediaDevices
+              .getUserMedia({audio: true, video: false})
+              .then(handleSuccess);
+          }
 
-          recorder.ondataavailable = (event) => {
-            console.log("data event:", event)
-            data.push(event.data)
-          };
+          const handleSuccess = async function (stream) {
+            console.log("micro input", stream)
+            recorder = new MediaRecorder(stream);
+            const data: any = [];
 
-          recorder.start(Math.round(16000 * 0.02));
-
-          const stopped = new Promise((resolve, reject) => {
-            recorder.onstop = resolve;
-            recorder.onerror = (event) => {
-              reject(event.name)
+            recorder.ondataavailable = (event) => {
+              console.log("data event:", event)
+              data.push(event.data)
+              if (event.data.size > 0 && socket.readyState == 1) {
+                socket.send(event.data)
+              }
             };
-          });
 
-          Promise.all([stopped]).then(() => {
-            console.log("data:", data)
+            recorder.start(1000);
 
-            // Stop the microphone tracks
-            const tracks = stream.getTracks();
-            tracks.forEach(track => track.stop());
-          });
-        };
+            socket.onmessage = (message) => {
+              const transcript = message.data
+              if (transcript) {
+                console.log('transcript', transcript)
+              }
+            }
 
-        navigator.mediaDevices
-          .getUserMedia({audio: true, video: false})
-          .then(handleSuccess);
+            socket.onclose = () => {
+              console.log({ event: 'onclose' })
+            }
+
+            socket.onerror = (error) => {
+              console.log({ event: 'onerror', error })
+            }
+
+            const stopped = new Promise((resolve, reject) => {
+              recorder.onstop = resolve;
+              recorder.onerror = (event) => {
+                reject(event.name)
+              };
+            });
+
+            Promise.all([stopped]).then(() => {
+              console.log("data:", data)
+
+              // Stop the microphone tracks
+              const tracks = stream.getTracks();
+              tracks.forEach(track => track.stop());
+            });
+          };
         }
       );
     },
